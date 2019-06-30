127136f031c134bf60752a5d775e03d14b53cc83
Egor Lepikhin <lepikhin.egor@yandex.ru>
2019-06-30 14:34:13 +0300

    switch to code based web configuration

---
+++ src/main/java/config/WebConfig.java
@@ -0,0 +1,33 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+	@Bean
+	public ViewResolver getViewResolver() {
+		FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
+		
+		viewResolver.setOrder(1);
+		viewResolver.setPrefix("");
+		viewResolver.setSuffix(".ftl");
+		viewResolver.setContentType("text/html; charset=windows-1251");
+		
+		return viewResolver;
+	}
+	@Bean
+	public FreeMarkerConfigurer getFreeMarkerConfigurer() {
+		FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+		configurer.setTemplateLoaderPath("/WEB-INF/pages");
+		
+		return configurer;
+	}
+	
+}
