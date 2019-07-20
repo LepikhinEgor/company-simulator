9e35294bc2264467d4996e65b150f596953fa113
Egor Lepikhin <lepikhin.egor@yandex.ru>
2019-07-20 18:38:13 +0300

    add exceptions loggin to logging aspect

--- src/main/java/aspects/LoggingAspect.java
+++ src/main/java/aspects/LoggingAspect.java
@@ -1,6 +1,8 @@
 package aspects;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -22,5 +24,25 @@ public class LoggingAspect {
 		}
 		
 		logger.info(logMessage);
+	}
+	
+	@Around("execution(* *(..)) && @annotation(aspects.annotations.Loggable)")
+	public void logMethod(ProceedingJoinPoint joinPoint) {
+		Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+		
+		String methodName = joinPoint.getSignature().getName();
+		String logMessage = "Invoked method " + methodName;
+		logMessage += "\n Args: ";
+		for(Object arg:joinPoint.getArgs()) {
+			logMessage += arg.toString() + " ";
+		}
+		
+		logger.info(logMessage);
+		
+		try {
+			joinPoint.proceed();
+		} catch (Throwable e) {
+			logger.error("Method " + methodName + "throws exception", e);
+		}
 	}
 }
