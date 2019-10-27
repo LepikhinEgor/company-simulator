5636b302a15387bd002ae777f083cf616c718d4a
Egor Lepikhin <lepikhin.egor@yandex.ru>
2019-10-27 17:59:21 +0300

    swith to spring 5

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -1,7 +1,10 @@
 package config;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -9,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+import freemarker.template.TemplateException;
 
 @Configuration
 @EnableWebMvc
@@ -25,7 +30,7 @@ public class WebConfig {
 		return viewResolver;
 	}
 	@Bean
-	public FreeMarkerConfigurer getFreeMarkerConfigurer() {
+	public FreeMarkerConfigurer getFreeMarkerConfigurer() throws IOException, TemplateException {
 		FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
 		configurer.setTemplateLoaderPath("/WEB-INF/pages");
 		
@@ -34,7 +39,7 @@ public class WebConfig {
 	
 	@Bean
 	WebMvcConfigurer webConfigurer() {
-		return new WebMvcConfigurerAdapter() {
+		return new WebMvcConfigurer() {
 			@Override
 			public void addResourceHandlers(ResourceHandlerRegistry registry) {
 				registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
